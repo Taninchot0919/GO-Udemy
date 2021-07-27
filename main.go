9dfc765ad4475eea86aa411d2775d47803d50df7
Taninchot Phuwaloertthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-07-27 22:12:27 +0700

    [UPDATE] Channels

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/taninchot0919/learning-go/helpers"
 )
 
-func main() {
-	fmt.Println("Hello Module")
+const numPool = 10
 
-	var myVar helpers.SomeType
-	myVar.TypeName = "Taninchot"
-	myVar.TypeNumber = 20
-	fmt.Println(myVar)
+func CalculateValue(intChanel chan int) {
+	randomNumber := helpers.RandomNumber(numPool) // ส่งเลขไป random แล้วนำกลับมาให้ randomNumber
+	intChanel <- randomNumber                     // เหมือนเป็นการเอาค่ามาใส่ให้กับ chanel ต้องใช้เครื่องหมาย <- ใช้ = ไม่ได้
+}
+func main() {
+	intChanel := make(chan int) // เป็นการสร้าง chanel ของ int ขึ้นมา
+	defer close(intChanel)      // defer คือรอทุกตัวรันเสร็จค่อยทำงาน
+
+	go CalculateValue(intChanel) // ใช้ go routine 
+
+	num := <-intChanel // เหมือนเป็นการเอาเลขจาก chanel มาใส่ตัวแปร num
+	fmt.Println(num)
 }
