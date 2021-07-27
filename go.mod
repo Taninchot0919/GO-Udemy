c294a0c87f13d0ed6787d3ce47cdf8d2de54c07e
Taninchot Phuwaloertthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-07-27 21:52:27 +0700

    [UPDATE] Packages

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taninchot0919/learning-go
+
+go 1.16
--- main.go
+++ main.go
@@ -1,59 +1,15 @@
 package main
 
-import "fmt"
-
-type Animal interface {
-	Says() string
-	NumberOfLeg() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+import (
+	"fmt"
+	"github.com/taninchot0919/learning-go/helpers"
+)
 
 func main() {
-	dog := Dog{
-		Name:  "Sam",
-		Breed: "German Shepperd",
-	}
+	fmt.Println("Hello Module")
 
-	printInfo(&dog)
-
-	gorilla := Gorilla{
-		Name: "Ling",
-		Color: "Brown",
-		NumberOfTeeth: 38,
-	}
-
-	printInfo(&gorilla)
-}
-
-func printInfo(a Animal)  {
-	fmt.Println("This animals say",a.Says(),"and has",a.NumberOfLeg(),"legs")
-}
-
-// -------- Interface --------
-// โดยเจ้า interface ในนี้เนี่ยก็เหมือนกันกับ interface ใน java เลย
-// พวกที่เป็น receiver ส่วนใหญ่แล้วจะเป็น pointer ดังนั้นควรใช้แบบนี้ดีกว่าเพื่อที่เป็น best practice
-func (d *Dog) Says() string {
-	return "Booooooooo"
-}
-
-func (d *Dog) NumberOfLeg() int {
-	return 4
-}
-
-func (g *Gorilla) Says() string {
-	return "Urghhhhhhh"
-}
-
-func (g *Gorilla) NumberOfLeg() int {
-	return 2
+	var myVar helpers.SomeType
+	myVar.TypeName = "Taninchot"
+	myVar.TypeNumber = 20
+	fmt.Println(myVar)
 }
