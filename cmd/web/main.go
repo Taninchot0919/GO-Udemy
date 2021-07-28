7cab696f550977efa34900053f7a5a88ac02ac23
Taninchot Phuwaloertthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-07-28 21:35:59 +0700

    [UPDATE] set app to wide confing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/taninchot0919/learning-go/pkg/handlers"
+	"log"
 	"net/http"
+
+	"github.com/taninchot0919/learning-go/pkg/config"
+	"github.com/taninchot0919/learning-go/pkg/handlers"
+	"github.com/taninchot0919/learning-go/pkg/render"
 )
 
 const portNumber = ":9000"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
